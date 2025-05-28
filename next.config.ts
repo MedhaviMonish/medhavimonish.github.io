23d1bd88edc142e4bc2f5476896cb9657d6f63b9
Medhavi Monish <monishmedhavi@gmail.com>
2025-05-28 10:51:00 +0530

    tried to fix path

--- next.config.ts
+++ next.config.ts
@@ -3,8 +3,8 @@ const isExport = process.env.EXPORT === 'true';
 
 const nextConfig = {
   output: isExport ? 'export' : undefined,
-  basePath: isExport ? "/medhavimonish.github.io" : "",
-  assetPrefix: isExport ? "/medhavimonish.github.io/" : "",
+  basePath: isExport ? "" : "",
+  assetPrefix: isExport ? "" : "",
   trailingSlash: true,
 };
 
